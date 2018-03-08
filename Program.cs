5fb98e08ec31f02f87709bd067317f8d2cf9822d
Denneil-Brooking <brookd@g.toiohomai.ac.nz>
2018-03-08 16:41:37 +1300

    done

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = "";
+            var inputnum = 0;
+            //Ask for number
+            Console.WriteLine("Please enter a number, press enter when done?");
+            //User input of number.
+            num = Console.ReadLine();
+            Console.WriteLine(Environment.NewLine);
+            //Input write
+            Console.WriteLine($"Your Number is {num}");
+            //loop it
+            var isNumber = int.TryParse(num, out inputnum);
+            for (var table = 0; table <= 12; table++)
+            {
+                //Print input times table up to 12 
+                Console.WriteLine(inputnum * table);
+            }
+            Console.WriteLine(Environment.NewLine);
+            for (var table = 0; table <= 12; table++)
+            {
+                //Print the stages
+                Console.WriteLine("Stage {0}*{1}={2}", num, table, inputnum * table);
+            }
+            //Close program
+            Console.ResetColor();
+            Console.WriteLine("Press <Enter> to quit the program");
+            Console.ReadKey();
+            //done..
+
+
+        }
+    }
+}
